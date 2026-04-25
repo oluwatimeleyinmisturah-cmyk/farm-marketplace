7f0fb3fbac07008ebc396ac097d153827b4b49f2
Misty <oluwatimeleyinmisturah@gmail.com>
2026-04-25 18:15:41 +0100

    fix csrf trusted origins

--- farm_market_system/settings.py
+++ farm_market_system/settings.py
@@ -14,6 +14,9 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.onrender.com'
+]
 
 # ─────────────────────────────
 # APPLICATIONS
