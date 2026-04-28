6d108277864c6038de654884c945e4d89a32a330
Misty <oluwatimeleyinmisturah@gmail.com>
2026-04-28 07:58:55 +0100

    Add superuser creation in app ready method
    Create a superuser 'OlaWale' if it does not exist.

--- marketplace/apps.py
+++ marketplace/apps.py
@@ -1,9 +1,17 @@
 from django.apps import AppConfig
- 
- 
+
 class MarketplaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'marketplace'
- 
+
     def ready(self):
         import marketplace.signals
+
+        from django.contrib.auth.models import User
+
+        if not User.objects.filter(username='OlaWale').exists():
+            User.objects.create_superuser(
+                'OlaWale',
+                'thesanniolawales@gmail.com',
+                'JeliliSanni'
+            )
