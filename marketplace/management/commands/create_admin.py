4cd74f9fd6d52792a4b9eaa1449f8ec2e90fd489
Misty <oluwatimeleyinmisturah@gmail.com>
2026-04-28 09:00:42 +0100

    Update create_admin.py

--- marketplace/management/commands/create_admin.py
+++ marketplace/management/commands/create_admin.py
@@ -1,1 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
 
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='OlaWale').exists():
+            User.objects.create_superuser(
+                'OlaWale',
+                'thesanniolawales@gmail.com',
+                'JeliliSanni'
+            )
+            self.stdout.write("Admin created")
+        else:
+            self.stdout.write("Admin already exists")
