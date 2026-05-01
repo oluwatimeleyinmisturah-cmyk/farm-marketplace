f45a4fd5d2e03c5ee1310d4e3bcc5b1840179c93
Misty <oluwatimeleyinmisturah@gmail.com>
2026-05-01 20:29:40 +0100

    Add email timeout and disable SSL for email settings

--- farm_market_system/settings.py
+++ farm_market_system/settings.py
@@ -128,13 +128,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp.gmail.com'
 EMAIL_PORT          = 587
 EMAIL_USE_TLS       = True
+EMAIL_TIMEOUT       = 10
+EMAIL_USE_SSL       = False
 EMAIL_HOST_USER     = 'thesanniolawales@gmail.com'
 EMAIL_HOST_PASSWORD = 'galkgblvbjxianmn'
 DEFAULT_FROM_EMAIL  = f'Farm Market Africa <{EMAIL_HOST_USER}>'
 
 CONTACT_EMAIL = 'thesanniolawales@gmail.com'
-EMAIL_TIMEOUT = 10
-EMAIL_USE_SSL = False
+
 
 
 # ─────────────────────────────
